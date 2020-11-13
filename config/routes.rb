c730e4087602557773aa2ad197ceaa2f3ec5a538
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-11-13 14:01:53 +0000

    Chore: Add Posts controller and basic views, test for status 200

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'post/create'
   get 'airlock/index'
   root 'airlock#index'
+
+  resources :posts
+  get 'post/create'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+
+  describe "GET /posts" do
+    it "Responds with status 200" do
+      get "/posts/"
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /posts/new" do
+    it "Responds with status 200" do
+      get "/posts/new"
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
