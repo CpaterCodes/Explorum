649c60585fee3b64e0f1022f0d8908f76ef7addb
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-01-13 13:17:01 +0000

    Feat: Implement formatted timestamps for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  def format_date(date)
+    return "#{date.day}|#{date.month}|#{date.year}"
+  end
+
+  def format_time(time)
+    output = "#{time.hour}:"
+    output += "0" if time.min < 10
+    output += "#{time.min}"
+    return output
+  end
+
 end
