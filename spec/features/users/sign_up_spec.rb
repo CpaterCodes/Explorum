ad4c71cb6c38d88ca988b8af0de63c46681d8135
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-01-20 19:21:14 +0000

    Test: Write first test for User features

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
-
+require_relative 'users_spec_helper'
 RSpec.describe "Sign up to be a user", type: :feature do
 
+  scenario "User can sign up" do
+    full_sign_up('ExampleUser', 'user@example.com', 'p4ssw0rd')
+    expect(page).to have_content('Current User: ExampleUser')
+  end
 end
