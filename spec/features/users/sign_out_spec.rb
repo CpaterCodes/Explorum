dd6965b7f980cb66298d9feefc33dca37c2bea29
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-01-20 14:38:24 +0000

    Test: Initialise test files for user-related features

---
+++ spec/features/users/sign_out_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Sign out of session as user", type: :feature do
+
+end
---
+++ spec/features/users/sign_up_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Sign up to be a user", type: :feature do
+
+end
