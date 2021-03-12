0580ae7b911eadb3a14d19fd119bc1e1465bef17
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-03-12 11:51:37 +0000

    Feat: Comments now have names and timestamps. Also a migration of methods for time and date formatting to enable use by both posts and comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def date(date)
+    return "#{date.day}|#{date.month}|#{date.year}"
+  end
+
+  def time(time)
+    output = "#{time.hour}:"
+    output += "0" if time.min < 10
+    output += "#{time.min}"
+    return output
+  end
+
+  def author_of(content)
+    return content.user ? content.user.username : 'n/a'
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,15 +5,4 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 300 }
   validates :content, length: { maximum: 50 }, if: -> { user_id.nil? }
 
-  def format_date(date)
-    return "#{date.day}|#{date.month}|#{date.year}"
-  end
-
-  def format_time(time)
-    output = "#{time.hour}:"
-    output += "0" if time.min < 10
-    output += "#{time.min}"
-    return output
-  end
-
 end
